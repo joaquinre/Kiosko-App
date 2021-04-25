1b4a3ae89bffa0c0342f17d79ea4e94e4506e4e6
Joaquin Re <62251875+joaquinre@users.noreply.github.com>
2021-04-25 19:34:13 -0300

    Add files via upload

---
+++ app/http/controllers/customers/orderController.js
@@ -0,0 +1,51 @@
+const Order = require('../../../models/order')
+const moment = require('moment')
+function orderController() {
+    return {
+        store(req, res) {
+            const { curso } = req.body
+            if (!curso) {
+                req.flash('error', 'All fields are required')
+                return res.redirect('/cart')
+            }
+
+            const order = new Order({
+                customerId: req.user._id,
+                // customerName: req.user,
+                items: req.session.cart.items,
+                curso,
+            })
+            order.save().then(result => {
+                Order.populate(result, { path: 'customerId' }, (err, placedOrder) => {
+                    req.flash('success', 'Order placed successfully')
+                    delete req.session.cart
+                    // Emit
+                    const eventEmitter = req.app.get('eventEmitter')
+                    eventEmitter.emit('orderPlaced', placedOrder)
+                    return res.redirect('customer/orders')
+                })
+            }).catch(err => {
+                req.flash('error', 'Something went wrong')
+                return res.redirect('/cart')
+            })
+        },
+        async index(req, res) {
+            const orders = await Order.find({ customerId: req.user._id },
+                 null,
+                 { sort: { 'createdAt': -1 } })
+            res.header('Cache-control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0')
+            res.render('customers/orders', { orders: orders, moment: moment })
+            console.log(orders);
+        },
+        async show(req, res) {
+            const order = await Order.findById(req.params.id)
+            // authorize user
+            if (req.user._id.toString() === order.customerId.toString()) {
+                return res.render('customers/singleOrder', { order }) 
+            }
+            return res.redirect('/')
+        }
+    } 
+}
+
+module.exports = orderController
